3bd00a7743cbc797f818f625a94781e93824ed0c
Umar F Khawaja <khawaja.umar.farooq@gmail.com>
2016-05-06 02:53:17 +0100

    Started integrating the README.md generator from generator-node.

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,75 +7,77 @@ var _ = require('lodash');
 
 module.exports = yeoman.Base.extend({
 
-  constructor: function () {
-    yeoman.Base.apply(this, arguments);
+    constructor: function () {
+        yeoman.Base.apply(this, arguments);
 
-    this.argument('name', {
-      desc: 'Provide a name for the project',
-      type: String,
-      required: true
-    });
+        this.argument('name', {
+            desc: 'Provide a name for the project',
+            type: String,
+            required: true
+        });
 
-    this.argument('nameSuffix', {
-      desc: 'Allow a custom suffix to be added to the module name',
-      type: String,
-      defaults: 'API',
-      required: false
-    });
+        this.argument('nameSuffix', {
+            desc: 'Allow a custom suffix to be added to the module name',
+            type: String,
+            defaults: 'API',
+            required: false
+        });
 
-    this.option('skip-install', {
-      desc: 'Do not install dependencies',
-      type: Boolean,
-      defaults: false
-    });
+        this.option('skip-install', {
+            desc: 'Do not install dependencies',
+            type: Boolean,
+            defaults: false
+        });
 
-    this.name = _.upperFirst(_.camelCase(this.name));
-    this.nameSuffix = _.upperCase(this.nameSuffix);
-  },
+        this.name = _.upperFirst(_.camelCase(this.name));
+        this.nameSuffix = _.upperCase(this.nameSuffix);
+    },
 
-  prompting: function () {
-    var done = this.async();
+    prompting: function () {
+        var done = this.async();
 
-    this.log(yosay('Welcome to the kickass ' + chalk.red('generator-mongolia') + ' generator!'));
+        this.log(yosay('Welcome to the kickass ' + chalk.red('generator-mongolia') + ' generator!'));
 
-    var prompts = [{
-      type: 'list',
-      name: 'stackName',
-      message: 'Which stack would you like to use to create your REST API project?',
-      choices: ['json-api', 'Restify', 'Express'],
-      filter: val => {
-        return {
-          'json-api': 'json-api',
-          'Restify': 'restify',
-          'Express': 'express'
-        }[val];
-      }
-    }, {
-      type: 'confirm',
-      name: 'shouldAddAuth',
-      message: 'Would you like to add authentication?',
-      // when: answers => answers.odms && answers.odms.length !== 0
-    }];
+        var prompts = [{
+            type: 'list',
+            name: 'stackName',
+            message: 'Which stack would you like to use to create your REST API project?',
+            choices: ['json-api', 'Restify', 'Express'],
+            filter: val => {
+                return {
+                    'json-api': 'json-api',
+                    'Restify': 'restify',
+                    'Express': 'express'
+                }[val];
+            }
+        }, {
+            type: 'confirm',
+            name: 'shouldAddAuth',
+            message: 'Would you like to add authentication?',
+            // when: answers => answers.odms && answers.odms.length !== 0
+        }];
 
-    this.prompt(prompts, function (props) {
-      this.props = props;
+        this.prompt(prompts, function (props) {
+            this.props = props;
 
-      done();
-    }.bind(this));
-  },
+            done();
+        }.bind(this));
+    },
 
-  writing: function () {
-    let self = this;
+    default: function () {
+        this.composeWith('node', {}, {
+            local: require.resolve('generator-node/generators/readme')
+        });
+    },
 
-    this.sourceRoot(path.join(__dirname, './templates'));
-    this.processDirectory('.', '.', function (dest) {
-      return dest;
-    });
-  },
+    writing: function () {
+        let self = this;
 
-  install: function () {
-    if (!this.options['skip-install']) {
-      this.installDependencies();
+    },
+
+    install: function () {
+        if (!this.options['skip-install']) {
+            this.installDependencies();
+        }
     }
-  }
 });
