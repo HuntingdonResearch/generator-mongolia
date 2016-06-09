6b5f5c0eb2257f2d1b9546d0568c981faa5ba5f6
Umar Khawaja <umar.khawaja@iintegra.com>
2016-06-09 18:24:17 +0100

    Restarted developing the code for the generator.

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,80 +4,105 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var _ = require('lodash');
+var gitConfig = require('git-config');
 
 module.exports = yeoman.Base.extend({
 
-    constructor: function () {
-        yeoman.Base.apply(this, arguments);
+  constructor: function () {
+    yeoman.Base.apply(this, arguments);
 
-        this.argument('name', {
-            desc: 'Provide a name for the project',
-            type: String,
-            required: true
-        });
+    this.argument('projectName', {
+      type: String,
+      required: false
+    });
 
-        this.argument('nameSuffix', {
-            desc: 'Allow a custom suffix to be added to the module name',
-            type: String,
-            defaults: 'API',
-            required: false
-        });
+    this.option('installDependencies');
 
-        this.option('skip-install', {
-            desc: 'Do not install dependencies',
-            type: Boolean,
-            defaults: false
-        });
+  },
 
-        this.name = _.upperFirst(_.camelCase(this.name));
-        this.nameSuffix = _.upperCase(this.nameSuffix);
-    },
+  initializing: {
+    author: function () {
+      var gc = gitConfig.sync();
 
-    prompting: function () {
-        var done = this.async();
+      gc.user = gc.user || {};
 
-        this.log(yosay('Welcome to the kickass ' + chalk.red('generator-mongolia') + ' generator!'));
-
-        var prompts = [{
-            type: 'list',
-            name: 'stackName',
-            message: 'Which stack would you like to use to create your REST API project?',
-            choices: ['json-api', 'Restify', 'Express'],
-            filter: val => {
-                return {
-                    'json-api': 'json-api',
-                    'Restify': 'restify',
-                    'Express': 'express'
-                }[val];
-            }
-        }, {
-            type: 'confirm',
-            name: 'shouldAddAuth',
-            message: 'Would you like to add authentication?',
-            // when: answers => answers.odms && answers.odms.length !== 0
-        }];
-
-        this.prompt(prompts, function (props) {
-            this.props = props;
-
-            done();
-        }.bind(this));
-    },
-
-    default: function () {
-        this.composeWith('node', {}, {
-            local: require.resolve('generator-node/generators/readme')
-        });
-    },
-
-    writing: function () {
-        let self = this;
-
-    },
-
-    install: function () {
-        if (!this.options['skip-install']) {
-            this.installDependencies();
-        }
+      this.authorName = gc.user.name;
+      this.authorEmail = gc.user.email;
     }
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    var prompts = [{
+      type: 'input',
+      name: 'projectName',
+      message: 'What\'s the project\'s name?',
+      default: this.projectName
+    }, {
+      type: 'input',
+      name: 'authorName',
+      message: 'What\'s the author\'s name?',
+      default: this.authorName
+    }, {
+      type: 'input',
+      name: 'authorEmail',
+      message: 'What\'s the author\'s email?',
+      default: this.authorEmail
+    }, {
+      type: 'confirm',
+      name: 'isGithubHosted',
+      message: 'Is the project hosted on Github?',
+      default: true
+    }, {
+      when: function (response) {
+        return response.isGithubHosted;
+      },
+      type: 'input',
+      name: 'githubUsername',
+      message: 'What\'s the author\'s Github username?',
+      default: this.githubUsername
+    }];
+
+    this.prompt(prompts, function (props) {
+
+      this.projectName = props.projectName;
+      this.authorName = props.authorName;
+      this.authorEmail = props.authorEmail;
+      this.isGithubHosted = props.isGithubHosted && !!props.githubUsername;
+      this.githubUsername = props.githubUsername;
+      this.options.installDependencies = !!this.options.installDependencies;
+
+      done();
+    }.bind(this));
+  },
+
+  configuring: function () {
+  },
+
+  default: function () {
+    this.log('projectName: ' + this.projectName);
+    this.log('authorName: ' + this.authorName);
+    this.log('authorEmail: ' + this.authorEmail);
+    this.log('isGithubHosted: ' + this.isGithubHosted);
+
+    if (this.isGithubHosted && !!this.githubUsername) {
+      this.log('githubUsername: ' + this.githubUsername);
+    }
+
+    this.log("installDependencies: " + this.options.installDependencies);
+  },
+
+  writing: function () {
+  },
+
+  conflicts: function () {
+  },
+
+  install: function () {
+  },
+
+  end: function () {
+  }
+
 });
