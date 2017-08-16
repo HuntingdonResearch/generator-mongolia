df0386ab51d908ebc76216d2b6f69c2ebf196809
Umar F Khawaja <khawaja.umar.farooq@gmail.com>
2017-08-16 17:32:18 +0100

    Added prototyping code playing with esprima.

--- generators/api-config/index.js
+++ generators/api-config/index.js
@@ -3,10 +3,71 @@
 const _ = require('lodash');
 const Generator = require('yeoman-generator');
 
-const esprima = require('esprima');
-const estemplate = require('estemplate');
-const estraverse = require('estraverse');
-const escodegen = require('escodegen');
+const test = (fileName) => {
+  let readFile = () => require('fs').readFileSync(fileName, 'utf-8');
+  let parseModule = require('esprima').parseModule;
+  let parseScript = require('esprima').parseScript;
+  let parseQuery = require('esquery').parse;
+  let matchQuery = require('esquery').match;
+  let generateCode = require('escodegen').generate;
+  let formatTree = (sourceTree) => JSON.stringify(sourceTree, undefined, "  ");
+  let printLog = (message) => console.log(message);
+
+  let makeLiteral = (value) => {
+    return {
+      type: 'Literal',
+      value: value
+    };
+  };
+
+  let sourceCode = readFile();
+  let sourceTree = parseModule(sourceCode);
+  let sourceQuery = parseQuery('[id.name="config"]');
+  let sourceMatches = matchQuery(sourceTree, sourceQuery);
+
+  if (sourceMatches.length !== 1) {
+    printLog("Too few or too many config declarations found");
+
+    return;
+  }
+
+  let sourceMatch = sourceMatches[0];
+
+  let initObjectExpression = sourceMatch['init'];
+  let initObjectProperties = initObjectExpression['properties'];
+
+  for (let i = 0; i < initObjectProperties.length; i++) {
+    let initObjectProperty = initObjectProperties[i];
+
+    let sectionName = initObjectProperty['key']['name'];
+    let sectionValue = initObjectProperty['value'];
+    let sectionInitializer;
+
+    switch(sectionValue.type) {
+      case 'CallExpression':
+      {
+        sectionInitializer = initObjectProperty['value']['arguments'];
+
+        if (sectionInitializer.length === 1) {
+          sectionInitializer = sectionInitializer[0];
+        } else if (sectionInitializer.length === 0) {
+          sectionInitializer = undefined;
+        } else {
+          printLog('Too few or too many section initializers for ' + sectionName);
+        }
+      }
+        break;
+    }
+
+    if (!_.isNil(sectionInitializer)) {
+      printLog(generateCode(sectionInitializer));
+    }
+
+    printLog(generateCode(sectionValue));
+
+    // printLog(sectionInitializer ? escodegen.generate(sectionInitializer) : "");
+  }
+};
 
 class MongoliaApiConfigGenerator extends  Generator {
   prompting() {
